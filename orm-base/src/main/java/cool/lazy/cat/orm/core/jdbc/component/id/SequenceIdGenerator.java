a20f10b4c73d36556e1c7f6484943dd399b8351a
马昊 <2628452503@qq.com>
2022-05-20 20:18:59 +0800

    钩子

---
+++ orm-base/src/main/java/cool/lazy/cat/orm/core/jdbc/component/id/SequenceIdGenerator.java
@@ -0,0 +1,10 @@
+package cool.lazy.cat.orm.core.jdbc.component.id;
+
+import cool.lazy.cat.orm.base.component.id.IdGenerator;
+
+/**
+ * @author: mahao
+ * @date: 2022-05-20 20:16
+ */
+public interface SequenceIdGenerator extends IdGenerator {
+}
