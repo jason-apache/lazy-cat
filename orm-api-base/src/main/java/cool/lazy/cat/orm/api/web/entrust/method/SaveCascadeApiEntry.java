ef4a024bc4f91d1aad2055ea245255abbaaf5c46
马昊 <2628452503@qq.com>
2022-05-20 19:23:47 +0800

    钩子接口

---
+++ orm-api-base/src/main/java/cool/lazy/cat/orm/api/web/entrust/method/SaveCascadeApiEntry.java
@@ -0,0 +1,10 @@
+package cool.lazy.cat.orm.api.web.entrust.method;
+
+import cool.lazy.cat.orm.api.base.method.ApiMethodEntryHook;
+
+/**
+ * @author: mahao
+ * @date: 2021/7/12 13:28
+ */
+public interface SaveCascadeApiEntry extends ApiMethodEntryHook {
+}
